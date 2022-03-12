d3412bfbcf681c88a8a01d0a88edebe5e0735b90
nguyenngocquang <19dh110483@st.huflit.edu.vn>
2022-03-12 13:46:24 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_TH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello my name Nguyen Ngoc Quang - 19DH110483");
+        }
+    }
+}
